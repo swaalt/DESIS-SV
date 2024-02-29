90a47f38279ac4268dce3aa6fc83326892f23fa2
Sebastián Araya <120288505+swaalt@users.noreply.github.com>
2024-02-29 11:20:07 -0300

    Se agregó validación por RUT.

--- Helper/validateHelper.php
+++ Helper/validateHelper.php
@@ -11,6 +11,7 @@ $fullName = $_POST['voter-full-name'];
 if (empty($fullName)) {
     $errors['voter-full-name'] = 'El campo Nombre y Apellido no puede estar en blanco.';
 }
+
 $alias = $_POST['voter-nickname'];
 if (empty($alias)) {
     $errors['voter-nickname'] = 'El campo Alias no puede estar en blanco.';
@@ -23,6 +24,12 @@ if (empty($alias)) {
 $rut = $_POST['voter-rut'];
 if (!preg_match('/^\d{7,8}-[0-9Kk]$/', $rut)) {
     $errors['voter-rut'] = 'El RUT debe estar escrito en el siguiente formato "xxxxxxxx-x".';
+} else {
+    // Validar RUT
+    $rutValido = validarRut($rut);
+    if (!$rutValido) {
+        $errors['voter-rut'] = 'El RUT no es válido.';
+    }
 }
 
 $email = $_POST['voter-email'];
@@ -50,6 +57,33 @@ if (empty($candidateId)) {
     $errors['voter-candidate'] = 'Seleccione un candidato.';
 }
 
+// Función para validar RUT. 
+function validarRut($rut) {
+    $rut = preg_replace('/[^0-9kK]/', '', $rut);
+    if (strlen($rut) != 9) {
+        return false;
+    }
+
+    $parteNumerica = substr($rut, 0, -1);
+    $verificador = strtoupper(substr($rut, -1));
+
+    $factor = 2;
+    $suma = 0;
+
+    for ($i = strlen($parteNumerica) - 1; $i >= 0; $i--) {
+        $suma += intval($parteNumerica[$i]) * $factor;
+        $factor = $factor == 7 ? 2 : $factor + 1;
+    }
+
+    $resto = $suma % 11;
+    $dvEsperado = $resto == 0 ? 0 : 11 - $resto;
+
+    if ($dvEsperado == 10) {
+        return ($verificador == 'K');
+    } else {
+        return ($verificador == $dvEsperado);
+    }
+}
 
 // Comprobar si hay errores en caso de haber, devuelve el error en AJAX, de lo contrario, entra al success.
 if (!empty($errors)) {
